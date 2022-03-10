eaddc5cd4f2f6392b51923ca2d3cf331f7512cd6
RedcepP <100447003+RedcepP@users.noreply.github.com>
2022-03-10 01:27:04 +0000

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,17 +1,17 @@
 local config = {
-    npcFrequencia = 1.0,
-    veiculosFrequencia = 1.0,
+    npc = 1.0,
+    veiculos = 1.0,
 }
 
 
 Citizen.CreateThread(function()
     while true do
         Citizen.Wait(0)
-        SetPedDensityMultiplierThisFrame(config.npcFrequencia)
-        SetScenarioPedDensityMultiplierThisFrame(config.npcFrequencia, config.npcFrequencia)
-        SetRandomVehicleDensityMultiplierThisFrame(config.veiculosFrequencia)
-        SetParkedVehicleDensityMultiplierThisFrame(config.veiculosFrequencia)
-        SetVehicleDensityMultiplierThisFrame(config.veiculosFrequencia)
+        SetPedDensityMultiplierThisFrame(config.npc)
+        SetScenarioPedDensityMultiplierThisFrame(config.npc, config.npc)
+        SetRandomVehicleDensityMultiplierThisFrame(config.veiculos)
+        SetParkedVehicleDensityMultiplierThisFrame(config.veículos)
+        SetVehicleDensityMultiplierThisFrame(config.veiculos)
 
     end 
 end)
